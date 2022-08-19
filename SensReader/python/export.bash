0ff0a28f4d0b9a826de92332aff51e2505630b83
sgk-000 <shigeki.kobayashi619@gmail.com>
2022-08-19 19:50:28 +0900

    update export.bash

--- SensReader/python/export.bash
+++ SensReader/python/export.bash
@@ -1,12 +1,11 @@
 #! /bin/bash
-#conda activate scannet_export
 
 DATA_ROOT='/media/digital/samsung_4t/scannet'
 files=($(find $DATA_ROOT -type f | grep .sens))
+arr=()
 for file in "${files[@]}"; do
     filename=$(echo $file | rev | cut -d'/' -f 1 | rev)
     path=$(echo $file | sed -e "s/$filename//g")
-    echo $filename;
-    echo $path;
-    sudo python reader.py --filename $file --output_path $path
+    arr+=($file $path)
 done
+echo ${arr[@]} | xargs -n 2 | parallel --progress -u --colsep ' ' --no-notice -- sudo python reader.py --export_depth_images --export_color_images --export_poses --export_intrinsics --filename {1} --output_path {2}
